09111b8e6d77c62ae14323a8f831664ffb691dee
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-25 23:16:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,8 +25,8 @@ def assign_rooms(attendees)
 end
     
 def printer(attendees)
-  
-  puts "#{batch_badge_creator}, #{assign_attendees}"
+  attendees= []
+  puts "#"batch_badge_creator", "assign_attendees"""
 end
 
 
